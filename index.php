f985b44dc3b3e0e40a347b425f54db47ef8fab0b
Zachary Rosenlund <zrosenlund@mail.greenriver.edu>
2018-01-12 17:46:31 -0500

    added .htaccess files and new views for page paths

--- index.php
+++ index.php
@@ -11,10 +11,25 @@
     //Create an instance of the base class
     $f3 = Base::instance();
 
+    //Set debug level
+    $f3->set('DEBUG', 3);
+
     //Define a default route
     $f3->route('GET /', function() {
         $view = new View;
         echo $view->render('views/home.html');
+    }
+    );
+
+    $f3->route('GET /page1', function() {
+        $view = new View;
+        echo $view->render('views/page1.html');
+    }
+    );
+
+    $f3->route('GET /page2', function() {
+        $view = new View;
+        echo $view->render('views/page2.html');
     }
     );
 
